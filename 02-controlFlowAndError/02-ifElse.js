dfe7da62fe4fed596cb94c20b30876a633d83b17
Ingeborg Slegers <ingeborgfreda@gmail.com>
2019-06-06 09:45:40 -0400

    day 2 work

---
+++ 02-controlFlowAndError/02-ifElse.js
@@ -0,0 +1,63 @@
+// * IF ELSE STATEMENT
+
+/* 
+if (condition) {
+  console.log();
+} else {
+  console.log();
+};
+*/
+
+let weather = 60;
+
+if (weather < 70) {
+  console.log('Wear a jacket');
+} else {
+  console.log('No jacket necessary!');
+};
+
+/* 
+* Challenge: 
+
+
+*Bronze: 
+* Write an if else statement that checks your name; 
+* If it is your name, console log '<name>'
+* If the name does not match, console.log 'Hello, what is your name?'
+
+* Silver:
+* If It is your name, console log 'Hello, my name is <name>'
+
+* Gold:
+* If it is not your name, console log 'Hello, is your name <name here> ?'
+*/
+
+let name = 'Jenny';
+if ('Ingeborg' == name) {
+  console.log('Hello, my name is', name);
+} else {
+  console.log('Hello, is your name', name);
+};
+
+/*
+Challenge: 
+**********
+* Use this string - let name = 'aUTuMN';
+
+* Bronze: 
+* Write an if else statement that looks at the first letter of a string. If it is uppercase, console.log the string; 
+* If it is not, console.log 'hey its not written correctly' 
+
+* Silver:
+* If the first letter of a string is uppercase, only console.log that letter.
+* If it is not, console.log the rest of the string Without the first letter, and change them to lowercase 
+
+* Gold: 
+* If the first letter of a string is uppercase, console.log that that first letter of a string  plus the rest of the string to lowercase
+* If it is not, console.log the first letter of the string to uppercase plus the rest of the string to lowercase
+
+*/
+
+
+
+
---
+++ 1-grammarTypes/06-literals.js
@@ -0,0 +1,23 @@
+// * Literals
+
+// * JavaScript Syntax defines two types of values: Fixed and variable values.
+// * Fixed values are called literals.
+// * Variable values are called variables.
+
+let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+console.log(days.toString());
+console.log(days.length);
+
+let tired = true;
+
+let soup = {
+  a: 'chicken noodle',
+  b: 'tomato',
+  c: 'beef and barley'
+};
+
+console.log(soup.c)
+
+let car = 'Honda';
+
+let dec = 10.2;
